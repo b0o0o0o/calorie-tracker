a7672b88aa0b9e370afdcea065d75c1cd9302a6f
b0o0o0o <b0o_o0@proton.me>
2025-06-07 16:54:30 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
       react(),
       tailwindcss(),
-  ],
-  base: '/calorie-tracker',
+    ],
+    base: env.NODE_ENV === 'production' ? '/calorie-tracker/' : '/',
+  }
 })
